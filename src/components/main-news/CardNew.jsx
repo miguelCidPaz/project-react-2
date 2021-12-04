1caaa66b0e012397a7ebf3addad66af4edda7fdc
miguelCidPaz <miguel.cid.paz@gmail.com>
2021-12-04 13:34:26 +0100

    ListNews

--- src/components/main-news/CardNew.jsx
+++ src/components/main-news/CardNew.jsx
@@ -3,13 +3,12 @@ import './styles.css'
 
 class CardNew extends Component {
     render() {
-        console.log(this.props.new)
         return (
             <div className="body-card">
                 <div className="card-text">
                     <p className="description-card-text">{this.props.new.description}</p>
                     <p className="categoria-card-text">{this.props.new.category}</p>
-                    <p className="cuerpo-card-text">{this.props.new.body}<button className="read-more">Leer Mas</button></p>
+                    <p className="cuerpo-card-text">{this.props.new.body}<button className="read-more">Leer Mas &gt;&gt;</button></p>
                     <p className="footer-card-text"><button className="comments-card-text">💬</button>{this.props.new.footer}</p>
                 </div>
 
