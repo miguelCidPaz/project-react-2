14ad2d2d9143bce39c1ab703c6b85794d2219806
santi <santimezquita@hotmail.es>
2021-12-08 19:41:01 +0100

    mejorado aside y responsive con grid

--- src/components/aside/AsideItems.js
+++ src/components/aside/AsideItems.js
@@ -5,8 +5,8 @@ class AsideItem extends React.Component {
         console.log(this.props);
         return <div className="asideItem">
             <h3>{`OFRECIDO POR ${this.props.element.tittle}`}</h3>
-            <img alt ='img' src={this.props.element.img}/>
-            <p>{this.props.element.text}</p>
+            <div className="asideItemBody"><img alt ='img' src={this.props.element.img}/>
+            <p>{this.props.element.text}</p></div>
             </div>
         }
 }
