24b9ac4c0c33fff3df97a8bd0c2790d8e8efbeb5
miguelCidPaz <miguel.cid.paz@gmail.com>
2021-12-12 18:01:13 +0100

    fetch-data

--- src/components/main-news/CardNew.jsx
+++ src/components/main-news/CardNew.jsx
@@ -7,9 +7,9 @@ class CardNew extends Component {
         return (
             <div className="body-card">
                 <div className="card-text">
-                    <p className="description-card-text">{this.props.new.description}</p>
-                    <p className="categoria-card-text">{this.props.new.category}</p>
-                    <p className="cuerpo-card-text">{this.props.new.content}<button className="read-more">Leer Mas &gt;&gt;</button></p>
+                    <p className="description-card-text">{this.props.new.title}</p>
+                    <p className="categoria-card-text">{this.props.new.author}</p>
+                    <p className="cuerpo-card-text">{this.props.new.description}<button className="read-more">Leer Mas &gt;&gt;</button></p>
                     <p className="footer-card-text"><button className="comments-card-text">💬</button>{this.props.new.footer}</p>
                 </div>
 
