0dbb9953c707ff627e19f445e507e0d863504c1b
miguelCidPaz <miguel.cid.paz@gmail.com>
2021-12-13 18:09:49 +0100

    fixs

--- src/components/main-news/CardNew.jsx
+++ src/components/main-news/CardNew.jsx
@@ -5,7 +5,7 @@ class CardNew extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            minimize: true
+            minimize: false
         }
         this.minimize = this.minimize.bind(this);
     }
