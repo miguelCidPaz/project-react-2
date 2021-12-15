c4e587b0673a2f5953859d19420db31d8cffc961
miguelCidPaz <miguel.cid.paz@gmail.com>
2021-12-15 23:41:32 +0100

    close preparado

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -4,13 +4,23 @@ import './style.css'
 class Banner extends Component {
     constructor(props) {
         super(props);
-        this.state = { ...this.props.ad }
+        this.state = {
+            ad: { ...this.props.ad },
+            /*  visible: true */
+        }
         console.disableYellowBox = true;
         this.adRandom = this.adRandom.bind(this);
+        /* this.closeAd = this.closeAd.bind(this); */
     }
 
+    /*     closeAd() {
+            this.setState(({
+                visible: !this.state.visible
+            }))
+        } */
+
     adRandom() {
-        const ads = Object.values({ ...this.state })
+        const ads = Object.values({ ...this.state.ad })
         const select = Math.floor(Math.random() * ads.length)
         return ads[select];
     }
@@ -18,21 +28,19 @@ class Banner extends Component {
 
     render() {
         const ad = this.adRandom()
-        return (
-            <div className="content-banner">
-                <div className="banner-frame">
-                    <img src={ad.img} alt="#" className="banner-img" />
-                </div>
-                <div className="banner-text">
-                    <p className="banner-message">{ad.text}</p>
-                    <p className="banner-trademark">{ad.mark}</p>
-                </div>
-                <div className="banner-action">
-                    <div className="banner-exit">X</div>
-                    <div className="banner-action-p">{ad.action}</div>
-                </div>
+        return <div className="content-banner">
+            <div className="banner-frame">
+                <img src={ad.img} alt="#" className="banner-img" />
             </div>
-        )
+            <div className="banner-text">
+                <p className="banner-message">{ad.text}</p>
+                <p className="banner-trademark">{ad.mark}</p>
+            </div>
+            <div className="banner-action">
+                <div className="banner-exit">X</div>
+                <div className="banner-action-p">{ad.action}</div>
+            </div>
+        </div>
     }
 }
 
