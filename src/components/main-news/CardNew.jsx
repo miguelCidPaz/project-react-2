4a3f1496d2a37f2b90e8b369ef00e6d13eb545a6
miguelCidPaz <miguel.cid.paz@gmail.com>
2021-12-16 02:40:34 +0100

    comments

--- src/components/main-news/CardNew.jsx
+++ src/components/main-news/CardNew.jsx
@@ -1,19 +1,26 @@
 import { Component } from "react";
+import NewsChat from './NewsChat'
 import './styles.css'
 
 class CardNew extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            minimize: false
+            minimize: false,
+            visualChat: false
         }
         this.minimize = this.minimize.bind(this);
+        this.openChat = this.openChat.bind(this);
+    }
+
+    openChat() {
+        this.setState(() => ({
+            visualChat: !this.state.visualChat
+        }))
     }
 
     minimize() {
         const min = this.state.minimize
-        console.log('es' + min)
-        console.log('en' + !min)
         this.setState(() => ({
             minimize: !min
         }))
@@ -22,17 +29,20 @@ class CardNew extends Component {
     render() {
         const photoDefault = "https://image.shutterstock.com/image-illustration/male-default-avatar-profile-gray-260nw-582509281.jpg";
         return (
-            <div className={this.state.minimize ? "body-card-minimize" : "body-card"}>
-                <div onClick={this.minimize} className="control"><p>{this.state.minimize ? "+" : "-"}</p></div>
-                {this.state.minimize ? <p className="pseudo-description">{this.props.new.title}</p> : <><div className="card-text">
-                    <p className="description-card-text">{this.props.new.title}</p>
-                    <p className="categoria-card-text">{this.props.category}</p>
-                    <p className="cuerpo-card-text">{this.props.new.description}<a href={this.props.new.url} target="_blank" rel="noreferrer" className="read-more">Leer Mas &gt;&gt;</a></p>
-                    <p className="footer-card-text"><button className="comments-card-text">💬</button>{this.props.new.footer}</p>
-                </div><div className="card-img">
-                        <img src={this.props.new.urlToImage !== null ? this.props.new.urlToImage : photoDefault} alt="#" />
-                    </div></>}
-            </div>
+            <>
+                {this.state.visualChat ? <NewsChat notice={this.props.new.title} /> : null}
+                <div className={this.state.minimize ? "body-card-minimize" : "body-card"}>
+                    <div onClick={this.minimize} className="control"><p>{this.state.minimize ? "+" : "-"}</p></div>
+                    {this.state.minimize ? <p className="pseudo-description">{this.props.new.title}</p> : <><div className="card-text">
+                        <p className="description-card-text">{this.props.new.title}</p>
+                        <p className="categoria-card-text">{this.props.category}</p>
+                        <p className="cuerpo-card-text">{this.props.new.description}<a href={this.props.new.url} target="_blank" rel="noreferrer" className="read-more">Leer Mas &gt;&gt;</a></p>
+                        <p className="footer-card-text"><button onClick={this.openChat} className="comments-card-text">💬</button>{this.props.new.footer}</p>
+                    </div><div className="card-img">
+                            <img src={this.props.new.urlToImage !== null ? this.props.new.urlToImage : photoDefault} alt="#" />
+                        </div></>}
+                </div>
+            </>
         )
     }
 }
