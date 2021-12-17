a8783d2fd262f1da12a1f5830ca96887e8af78c8
miguelCidPaz <miguel.cid.paz@gmail.com>
2021-12-17 19:15:22 +0100

    links category

--- src/components/header/components/NavHead.jsx
+++ src/components/header/components/NavHead.jsx
@@ -1,5 +1,10 @@
 import { Component } from "react";
 
+/**
+ * Esta etiqueta se encarga de devolver un valor entero segun su posicion al contenedor central App,
+ * De esta forma evitamos depender del html que podria ser editable desde fuera, unicamente trataremos el id
+ * que recibimos por prop.
+ */
 class NavHead extends Component {
     render() {
         return (
