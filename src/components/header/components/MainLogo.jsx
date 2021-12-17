ea37d05995c10d055852b189fb978921d4448859
miguelCidPaz <miguel.cid.paz@gmail.com>
2021-12-17 19:13:30 +0100

    logo

--- src/components/header/components/MainLogo.jsx
+++ src/components/header/components/MainLogo.jsx
@@ -1,6 +1,10 @@
 import { Component } from "react";
 import '../style.css';
 
+/**
+ * Simplemente contiene el logo, pero por si se necesitaba usar en algun otro sitio
+ * decidi aislarlo en una etiqueta a parte.
+ */
 class MainLogo extends Component {
     render() {
         return (
