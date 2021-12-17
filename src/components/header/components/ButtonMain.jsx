36ebbff3895470dd143d588f9f39121f6d1014a2
miguelCidPaz <miguel.cid.paz@gmail.com>
2021-12-17 19:10:56 +0100

    button reutilizable

--- src/components/header/components/ButtonMain.jsx
+++ src/components/header/components/ButtonMain.jsx
@@ -1,5 +1,12 @@
 import { Component } from "react";
 
+/**
+ * Este componente sera un boton reutilizable al que iremos cambiando tanto la clase
+ * como el texto que contiene, de esta forma podremos reutilizarlo las veces necesarias y 
+ * si se necesitase podriamos incluir hasta una funcion personalizada para cada uno.
+ * 
+ * Recibira la prop de buttontype que contendra el tipo(la clase) y el texto(la forma segun la fuente css)
+ */
 class ButtonMain extends Component {
     render() {
         return (
