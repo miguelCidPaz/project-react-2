f136db31af1542424e0ef11ffc43e2130f8fef29
miguelCidPaz <miguel.cid.paz@gmail.com>
2021-12-17 19:12:36 +0100

    Top content

--- src/components/header/components/HeaderMain.jsx
+++ src/components/header/components/HeaderMain.jsx
@@ -3,6 +3,9 @@ import '../style.css';
 import ButtonMain from "./ButtonMain";
 import MainLogo from "./MainLogo";
 
+/**
+ * Esta clase recogera 3 componentes, 2 buttonsmain y el mainLogo de Xataka
+ */
 class HeaderMain extends Component {
     render() {
         const buttons = Object.values({ ...this.props.mainButtons });
